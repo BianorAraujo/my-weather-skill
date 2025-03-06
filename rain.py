832e6b569059a040697a76af0f5a497fc67b355f
Bianor Araujo <bianor.araujo@gmail.com>
2025-03-06 11:47:33 +0000

    Changing format time do am/pm and add bye bye farewell

--- rain.py
+++ rain.py
@@ -9,28 +9,37 @@ class Rain:
 
             image = response["current"]["condition"]["icon"]
             current_time = response["location"]["localtime"].split(" ")[1]
-            #current_rain = response["current"]["condition"]["text"]
+            current_time_12 = Rain.convert_time_format(current_time)
             precipitation = response["current"]["precip_mm"]
-            is_raining = bool(precipitation > 0)
+            is_raining = bool(precipitation > 0.5)
 
             for hour in response["forecast"]["forecastday"][0]["hour"]:
+                
+                hour_24 = hour["time"].split(" ")[1]
+                hour_12 = Rain.convert_time_format(hour_24)
+
                 if is_raining:
                     is_raining_text = "It's raining!"
-                    if hour["time"].split(" ")[1] >= current_time and hour["precip_mm"] <= 0:
-                        next_event = f"The rain will stop around {hour["time"].split(" ")[1]}."
+
+                    if hour_12 >= current_time_12 and hour["precip_mm"] <= 0:
+                        next_event = f"The rain will stop around {hour_12}"
+                        break
                     else:
                         next_event = "It will rain all day"
                 else:
                     is_raining_text = "It's not raining!"
-                    if hour["time"].split(" ")[1] >= current_time and hour["precip_mm"] >= 0:
-                        next_event = f"The rain will start around {hour["time"].split(" ")[1]}."
+                    
+                    if hour_12 >= current_time_12 and hour["precip_mm"] > 0.5:
+                        next_event = f"The rain will start around {hour_12}"
+                        break
                     else:
-                        next_event = "There is no rain forecast for today."
+                        next_event = "There is no rain forecast for today"
+                
 
             if is_raining:
-                text = f"Now, in Dublin is raining. {next_event}"
+                text = f"In Dublin is raining. {next_event}"
             else:
-                text = f"Now, in Dublin is not raining. {next_event}"
+                text = f"In Dublin is not raining. {next_event}"
 
             alexa_response = {
                 "version": "1.0",
@@ -69,3 +78,16 @@ class Rain:
             }
 
             return jsonify(error_response)
+
+    @staticmethod 
+    def convert_time_format(hour24: str) -> str:
+        hour, minutes = hour24.split(':')
+        hour = int(hour)
+
+        period = 'AM' if hour < 12 else 'PM'
+
+        hour_12 = hour % 12
+        if hour_12 == 0:
+            hour_12 = 12
+        
+        return f"{hour_12}:{minutes} {period}"
